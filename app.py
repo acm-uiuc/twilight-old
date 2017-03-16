95c05e73a6548cda49ccbb31cea2bc901a6db64e
Eric Ahn <wchill1337@gmail.com>
2017-03-15 22:23:26 -0500

    Change defaults

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from plugin_manager import PluginManager
 
 def main():
     manager = PluginManager()
-    manager.load_plugin("epilepsy")
     manager.load_plugin("mood")
+    manager.load_plugin("epilepsy")
     manager.start()
 
 
