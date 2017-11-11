465868b164fb796c74bd879efc90f2b29130625b
Naren Dasan <naren@narendasan.com>
2017-11-11 17:14:58 -0600

    new firmware

--- PIC/firmware/firmware.ino
+++ PIC/firmware/firmware.ino
@@ -9,32 +9,49 @@ Thread Network = Thread();
 Thread DeviceI2C = Thread();
 ThreadController ctrlr = ThreadController();
 
+uint8_t counter = 0;
+
 void setup() {
-  
-  //Setup LED Thread 
-  setup_frame();
-  LEDs.enabled = true;
-  LEDs.setInterval(10);
-  LEDs.onRun(update_frame);
+    
+    //Setup LED Thread 
+    setup_frame();
+    LEDs.enabled = true;
+    LEDs.setInterval(10);
+    LEDs.onRun(update_frame);
 
-  //Setup Network Thread
-  /*setup_networking();
-  Network.enabled = true;
-  Network.setInterval(10);
-  Network.onRun(handle_network_msgs);*/
-  
-  //DeviceI2C
-  /*setup_i2c();
-  DeviceI2C.enable = true;
-  DeviceI2C.setInterval(10);
-  DeviceI2C.onRun(handle_i2c_msgs);*/
+    //Setup Network Thread
+    /*setup_networking();
+    Network.enabled = true;
+    Network.setInterval(10);
+    Network.onRun(handle_network_msgs);*/
+    
+    //DeviceI2C
+    /*setup_i2c();
+    DeviceI2C.enable = true;
+    DeviceI2C.setInterval(10);
+    DeviceI2C.onRun(handle_i2c_msgs);*/
 
-  ctrlr.add(&LEDs);
-  ctrlr.add(&Network);
-  ctrlr.add(&DeviceI2C);
+    ctrlr.add(&LEDs);
+    ctrlr.add(&Network);
+    ctrlr.add(&DeviceI2C);
 }
 
 void loop() {
-    ctrlr.run(); 
+    ctrlr.run();
+    if (counter % 3 == 0) {
+        frame.SetColor(0,0,255);
+    }
+    
+    else if (counter % 3 == 1) {
+        frame.SetColor(0,255,0);
+    }
+
+    else {
+        frame.SetColor(255,0,0);
+    }
+    counter++; 
 }
+
+
+
 
