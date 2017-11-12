9f9ca04ec24db8637addb2c751cd612a12daae38
rauhul <rauhul.varma@gmail.com>
2017-11-11 19:42:22 -0600

    initial control panel app structure

---
+++ iOS/twilight/APIServices/GrootService.swift
@@ -0,0 +1,16 @@
+//
+//  GrootService.swift
+//  twilight
+//
+//  Created by Rauhul Varma on 11/11/17.
+//  Copyright © 2017 acm. All rights reserved.
+//
+
+import Foundation
+import APIManager
+
+class GrootService: APIService {
+
+
+    
+}
---
+++ iOS/twilight/APIServices/GrootUsersService.swift
@@ -0,0 +1,17 @@
+//
+//  GrootUsersService.swift
+//  twilight
+//
+//  Created by Rauhul Varma on 11/11/17.
+//  Copyright © 2017 acm. All rights reserved.
+//
+
+import Foundation
+import APIManager
+
+class GrootUsersService: GrootService {
+
+
+
+}
+
